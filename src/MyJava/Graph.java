f045803ab37376b89e946ae25a39cd88e16f8871
WuJunde <izzy843794947@gmail.com>
2017-10-10 22:22:25 +0800

    Add files via upload

---
+++ src/MyJava/Graph.java
@@ -0,0 +1,333 @@
+package MyJava;
+
+import java.io.*;
+import java.util.*;
+class Node  {
+	String vertex=null;
+	Node nextNode=null;
+	int weight=0;
+	Node(){}
+	Node(String s){vertex=new String(s);}
+}
+public class Graph {	
+	final int MAX_SIZE=100;
+	List<Node> adjacencyList=new LinkedList<Node>();
+	Map<String,Integer> strIndex=new HashMap<String,Integer>();
+	List<String>pathMark=new LinkedList<String>();
+	int []visit=new int[MAX_SIZE];
+	List<String>path=new LinkedList<String>();
+	private Node randStart=null;
+	private int[][]randVisit=new int[MAX_SIZE][MAX_SIZE];
+	public Node randEnd=null;
+	public Graph(String fileName) throws IOException {
+		Creat_adjacencyList(fileName);
+	}
+	private   List<String> readText (String fileName) throws IOException {
+		File file=new File(fileName);
+		FileReader fr=new FileReader(file);
+		BufferedReader bufr=new BufferedReader(fr);
+		String s=new String();
+		String s1=new String();
+		while((s1=bufr.readLine())!=null)
+			s+=s1+"\n";
+		StringBuilder builder=new StringBuilder();
+		s=s.trim();
+		for(int i=0;i!=s.length();i++) {
+			char c=s.charAt(i);
+			if((c>='a'&&c<='z')) 
+				builder.append(c);
+			else if(c>='A'&&c<='z')
+			{
+				c=(char) (c+32);
+				builder.append(c);
+			}
+			else
+				builder.append(" ");
+		}
+		s=builder.toString();
+		List<String> list=new LinkedList<String>();
+		String[] arrString=s.split(" ");
+		for(int i=0;i!=arrString.length;i++) {
+			if(!arrString[i].equals("")) {
+				list.add(arrString[i]);
+			}
+		}
+		bufr.close();
+		return list;
+	}
+	private void Creat_adjacencyList(String fileName) throws IOException {
+		List<String>list=readText(fileName);
+		int count=0;
+		for(int i=0;i!=list.size();i++) {
+			if(!strIndex.containsKey(list.get(i)))
+			{
+				adjacencyList.add(new Node(list.get(i)));
+				strIndex.put(list.get(i), count++);
+			}
+		}
+		for(int i=1;i!=list.size();i++) {
+			String pstr=list.get(i-1);
+			String qstr=list.get(i);
+			Node temp=adjacencyList.get(strIndex.get(pstr)).nextNode;
+			while(temp!=null) {
+				if(temp.vertex.equals(qstr))
+					break;
+				temp=temp.nextNode;
+			}
+			if(temp!=null)
+				temp.weight++;
+			else {
+				temp=new Node(qstr);
+				temp.nextNode=adjacencyList.get(strIndex.get(pstr)).nextNode;
+				adjacencyList.get(strIndex.get(pstr)).nextNode=temp;
+				temp.weight++;
+				adjacencyList.get(strIndex.get(pstr)).weight++;
+			}
+		}
+	}
+	public void showDirectedGraph() {
+		StringBuilder format =new StringBuilder() ;
+		Iterator<Node> iter=adjacencyList.iterator();
+		while(iter.hasNext()) {
+			Node temp1=iter.next();
+			if(temp1!=null)
+			{
+				Node temp2=temp1.nextNode;
+				while(temp2!=null) {
+					format.append(temp1.vertex+"->"+temp2.vertex+"[label="+temp2.weight+"];");	
+					temp2=temp2.nextNode;
+				}
+			}
+		}
+		GraphViz.createDotGraph(format.toString(), "DotGraph");
+	}
+	public List<String> queryBridgeWords(String word1,String word2){
+		word1=word1.toLowerCase();
+		word2=word2.toLowerCase();
+		List<String>list =new LinkedList<String>() ;
+		String format=new String();
+		Iterator<Node> iter=adjacencyList.iterator();
+		Node temp=null;
+		Node temp1=null;
+		int flag1=0,flag2=0;
+		while(iter.hasNext()) {
+			temp=iter.next();
+			if(temp.vertex.equals(word1)) {
+				temp1=temp;
+				flag1=1;
+			}
+			if(temp.vertex.endsWith(word2))
+				flag2=1;
+		}
+		if(temp1!=null) temp1=temp1.nextNode;
+		while(temp1!=null) {
+			Node temp2=null;
+			temp2=adjacencyList.get(strIndex.get(temp1.vertex));
+			Node temp3=null;
+			if(temp2!=null) temp3=temp2.nextNode;
+			while(temp3!=null) {
+				if(temp3.vertex.equals(word2)) {
+					list.add(temp2.vertex);
+					break;
+				}
+				temp3=temp3.nextNode;
+			}
+			temp1=temp1.nextNode;
+		}
+		if(list.size()==0) {
+			if(flag1==0&&flag2==0) {
+				format="No \""+word1+"\" and \""+word2+"\" in the graph!";
+			}else if(flag1==0) {
+				format="No \""+word1+"\" in the graph!";
+			}else if(flag2==0) {
+				format="No \""+word2+"\" in the graph!";
+			}else {
+				format="No bridge words from \""+word1+"\" to \""+word2+"\"!";
+			}
+		}else if(list.size()==1){
+			format="The bridge words from \""+word1+"\""+" to \""+word2+"\" is: "+list.get(0);
+		}else {
+			format="The bridge words from \""+word1+"\""+" to \""+word2+"\" are: ";
+			for(int i=0;i!=list.size();i++) {
+				format+=list.get(i)+" ";
+			}
+		}
+		list.add(0,format);
+		return list;		
+	}
+	public String generateNewText(String inputText) {
+		inputText=inputText.toLowerCase();
+		String[] words=inputText.split(" ");
+		StringBuilder format=new StringBuilder();
+		for(int i=1;i!=words.length;i++) {
+			List<String> list=queryBridgeWords(words[i-1],words[i]);
+			format.append(words[i-1]+" ");
+			Random rand=new Random();
+			if(list.size()>1)
+			{
+				int k=rand.nextInt(list.size()-1);
+				format.append(list.get(k+1)+" ");
+			}
+		}
+		format.append(words[words.length-1]);
+		return format.toString();
+	}
+	void Initialize_path() {
+		pathMark.clear();
+		for(int i=0;i!=visit.length;i++) visit[i]=0;
+		path.clear();
+	}
+	String printPath(List<String> pathMark) {
+		StringBuilder builder=new StringBuilder();
+		int i=0;
+		for(;i!=pathMark.size()-1;i++)
+			builder.append(pathMark.get(i)+"->");
+		builder.append(pathMark.get(i));
+		return builder.toString();
+	}
+	void dfs(int shortestPathMartrix[][],String word1,String word2) {
+		visit[strIndex.get(word1)]=1;
+		pathMark.add(word1);
+		if(word1==word2) {
+			path.add(printPath(pathMark));
+		}
+		else {
+			int i=strIndex.get(word1);
+			for(int j=0;j!=adjacencyList.size();j++) {
+				String temp=adjacencyList.get(j).vertex;
+				if(shortestPathMartrix[i][strIndex.get(temp)]!=0&&visit[strIndex.get(temp)]!=1) {
+					dfs(shortestPathMartrix,temp,word2);
+				}
+			}
+		}
+		visit[strIndex.get(word1)]=0;
+		pathMark.remove(pathMark.size()-1);
+	}
+	public List<List<String>>calcShortestPath(String word1,String word2){
+		word1=word1.toLowerCase();
+		word2=word2.toLowerCase();
+		List<List<String>> shortestPathInfo= new LinkedList<List<String>>();
+		int flag1=0,flag2=0;
+		List<Integer> cost=new ArrayList<Integer>();
+		List<Integer> flag=new ArrayList<Integer>();
+		List<String> path=new ArrayList<String>();
+		int [][]shortestPathMartix=new int[MAX_SIZE][MAX_SIZE];
+		for(int i=0;i!=adjacencyList.size();i++) {
+			cost.add(99999);
+			flag.add(0);
+			path.add("");
+			if(adjacencyList.get(i).vertex.equals(word1))
+				flag1=1;
+			if(adjacencyList.get(i).vertex.equals(word2))
+				flag2=1;
+		}
+		if(word2.equals(""))
+			flag2=1;
+		List<String> list=new LinkedList<String>();
+		if(flag1==0&&flag2==0) {
+			list.add("No \""+word1+"\" and \""+word2+"\" in the graph!");
+			shortestPathInfo.add(list);
+		}else if(flag1==0) {
+			list.add("No \""+word1+"\" in the graph!");
+			shortestPathInfo.add(list);
+		}else if(flag2==0) {
+			list.add("No \""+word2+"\" in the graph!");
+			shortestPathInfo.add(list);
+		}else {
+			cost.set(strIndex.get(word1), 0);
+			path.set(strIndex.get(word1), word1);
+			String pathMark=word1;
+			for(int count=adjacencyList.size()-1;count!=0;count--) {			
+				int minCost=99999;
+				for(int i=0;i!=adjacencyList.size();i++) {
+					String str=adjacencyList.get(i).vertex;
+					if(flag.get(strIndex.get(str))!=1&&cost.get(strIndex.get(str))<minCost) {
+						minCost=cost.get(strIndex.get(str));
+						pathMark=str;
+					}
+				}
+				flag.set(strIndex.get(pathMark),1);
+				Node temp=adjacencyList.get(strIndex.get(pathMark)).nextNode;
+				while(temp!=null) {
+					int costW=cost.get(strIndex.get(temp.vertex));
+					if(flag.get(strIndex.get(temp.vertex))!=1&&minCost+temp.weight<costW) {					
+						shortestPathMartix[strIndex.get(pathMark)][strIndex.get(temp.vertex)]=temp.weight;
+						String prePathMark=path.get(strIndex.get(temp.vertex));
+						if(!prePathMark.equals(""))
+							shortestPathMartix[strIndex.get(prePathMark)][strIndex.get(temp.vertex)]=0;
+						cost.set(strIndex.get(temp.vertex), minCost+temp.weight);
+						path.set(strIndex.get(temp.vertex),pathMark);
+					}
+					else if(flag.get(strIndex.get(temp.vertex))!=1&&minCost+temp.weight==costW) {
+						shortestPathMartix[strIndex.get(pathMark)][strIndex.get(temp.vertex)]=temp.weight;
+					}
+					temp=temp.nextNode;
+				}			
+			}
+			if(word2.equals(""))
+			{
+				list.add("The shortest paths from \""+word1+"\" to the other words are: ");
+				shortestPathInfo.add(list);
+			}
+			for(int i=0;i!=adjacencyList.size();i++) {
+				String temp=adjacencyList.get(i).vertex;
+				if(!temp.equals(word1)) {
+					Initialize_path();
+					dfs(shortestPathMartix, word1, temp);
+					List<String>list2=new LinkedList<String>();
+					list2.addAll(this.path);
+					if(list2.size()==0)
+						list2.add("There is no shortest path from \""+word1+"\" to \""+temp+"\"");
+					shortestPathInfo.add(list2);
+					if(temp.equals(word2)) {
+						list2.add(0,"The shortest paths from  \""+word1+"\" to \""+word2+"\" are: ");
+						shortestPathInfo.add(0,list2);
+					}
+				}
+			}
+		}
+		return shortestPathInfo;
+	}
+
+	public void randInit() {
+		randStart=null;
+		randEnd=null;
+		randVisit=new int[MAX_SIZE][MAX_SIZE];
+	}
+	public String randomWalk() {
+		Random random=new Random();
+		if(randStart==null) {
+			int startPos=random.nextInt(adjacencyList.size());
+			randStart=adjacencyList.get(startPos);
+			return randStart.vertex;
+		}else {
+			Node temp=adjacencyList.get(strIndex.get(randStart.vertex));
+			List<Node>list=new LinkedList<Node>();
+			Node temp2=temp.nextNode;
+			while(temp2!=null) {
+				list.add(temp2);
+				temp2=temp2.nextNode;
+			}
+			if(list.size()!=0)
+			{
+				int nextPos=random.nextInt(list.size());
+				randStart=adjacencyList.get(strIndex.get(list.get(nextPos).vertex));
+				if(randVisit[strIndex.get(temp.vertex)][strIndex.get(randStart.vertex)]==1)
+				{
+					randEnd=randStart;
+					randStart=null;
+					return "";
+				}
+				else
+				{
+					randVisit[strIndex.get(temp.vertex)][strIndex.get(randStart.vertex)]=1;
+					return randStart.vertex;
+				}
+			}
+			else {
+				randStart=null;
+				return "";
+			}
+		}
+	}
+}
---
+++ src/MyJava/generateDialog.java
@@ -0,0 +1,62 @@
+package MyJava;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class generateDialog extends JDialog {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public generateDialog(Graph graph) {
+		Container container=getContentPane();
+		container.setLayout(new GridLayout(2, 1));
+		
+		JLabel inputLabel=new JLabel("请输入源文本",JLabel.CENTER);
+		JTextArea inputText=new JTextArea();
+		JButton generate=new JButton("转换");
+		
+		JPanel jp1=new JPanel();
+		jp1.setLayout(new GridLayout(1,	 3));
+		jp1.add(new JLabel());
+		jp1.add(new JLabel());
+		jp1.add(generate);
+		
+		JPanel jp2=new JPanel();
+		jp2.setLayout(new BorderLayout());
+		jp2.add(inputLabel, BorderLayout.NORTH);
+		jp2.add(inputText,BorderLayout.CENTER);
+		jp2.add(jp1,BorderLayout.SOUTH);
+		
+		JLabel outputLabel=new JLabel("转换文本如下",JLabel.CENTER);
+		JTextArea outputText=new JTextArea();
+		outputText.setEditable(false);
+		generate.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String s=graph.generateNewText(inputText.getText());
+				outputText.setText(s);
+			}
+		});
+		
+		JPanel jp3=new JPanel();
+		jp3.setLayout(new BorderLayout());
+		jp3.add(outputLabel, BorderLayout.NORTH);
+		jp3.add(outputText,BorderLayout.CENTER);
+		
+		container.add(jp2);
+		container.add(jp3);
+		setVisible(true);
+		Toolkit kit=Toolkit.getDefaultToolkit();
+		Dimension screenSize=kit.getScreenSize();
+		setBounds(screenSize.width/3, screenSize.height/3,500,400);
+		setResizable(false);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+
+	}
+}
