161f215567f4fdcd7ce724e1fe79edbd79b15d73
REKOJNOMED <819849784@qq.com>
2017-10-10 14:32:47 +0800

    first commit

---
+++ src/MyJava/randomDialog.java
@@ -0,0 +1,140 @@
+package MyJava;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class randThread extends Thread{
+	JTextArea textArea;
+	Graph graph;
+	public randThread(JTextArea textArea,Graph graph) {
+		this.textArea=textArea;
+		this.graph=graph;
+	}
+	public void run() {
+		StringBuilder builder=new StringBuilder();
+		String s=new String();
+		while(true) {
+			try {
+				sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			s=graph.randomWalk();
+			if(s.equals("")) {
+				if(graph.randEnd!=null)
+				builder.append(graph.randEnd.vertex+" ");
+				textArea.setText(builder.toString());
+				 try {  
+			            String data = builder.toString();  
+			  
+			            File file = new File("/Users/joker/Desktop/randomWalk.txt");  
+			            // if file doesnt exists, then create it  
+			            if (!file.exists()) {  
+			                file.createNewFile();  
+			            }  
+			  
+			            // true = append file  
+			            FileWriter fileWritter = new FileWriter(file);  
+			            fileWritter.write(data);  
+			            fileWritter.close();  
+			  
+			        } catch (IOException e) {  
+			            e.printStackTrace();  
+			        } 
+				JOptionPane.showMessageDialog(null, "已完成");
+				break;
+			}else {
+				builder.append(s+" ");
+				textArea.setText(builder.toString());
+				 try {  
+			            String data = builder.toString();  
+			  
+			            File file = new File("/Users/joker/Desktop/randomWalk.txt");  
+			            // if file doesnt exists, then create it  
+			            if (!file.exists()) {  
+			                file.createNewFile();  
+			            }  
+			  
+			            // true = append file  
+			            FileWriter fileWritter = new FileWriter(file);  
+			            fileWritter.write(data);  
+			            fileWritter.close();  
+			  
+			        } catch (IOException e) {  
+			            e.printStackTrace();  
+			        } 
+			}
+		}
+	}
+}
+@SuppressWarnings("serial")
+public class randomDialog extends JDialog {
+	static int flag=1;
+	randThread rth=null;
+	public randomDialog(Graph graph) {
+		Container container=getContentPane();
+		container.setLayout(new BorderLayout());
+		
+		JButton startButton=new JButton("开始");
+		JButton endButton=new JButton("终止");
+		
+		JPanel jp1=new JPanel();
+		jp1.setLayout(new GridLayout(1, 5));
+		jp1.add(new JLabel());
+		jp1.add(startButton);
+		jp1.add(new JLabel());
+		jp1.add(endButton);
+		jp1.add(new JLabel());
+		
+		JTextArea textArea=new JTextArea();
+		textArea.setEditable(false);
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		JScrollPane js=new JScrollPane(textArea);
+		startButton.addActionListener(new ActionListener() {
+			
+			@SuppressWarnings("deprecation")
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(rth!=null) rth.stop();
+				graph.randInit();
+				rth=new randThread(textArea,graph);
+				rth.start();
+			}
+		});
+		endButton.addActionListener(new ActionListener() {
+			
+			@SuppressWarnings("deprecation")
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(rth!=null) rth.stop();
+				
+				graph.randInit();
+				rth=null;
+			}
+		});
+	
+	
+		
+		container.add(js, BorderLayout.CENTER);
+		container.add(jp1, BorderLayout.SOUTH);
+		
+		setVisible(true);
+		Toolkit kit=Toolkit.getDefaultToolkit();
+		Dimension screenSize=kit.getScreenSize();
+		setBounds(screenSize.width/3, screenSize.height/3, 300, 200);
+		setResizable(false);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+	public static void main(String[]args) throws IOException {
+		
+	}
+}
---
+++ src/MyJava/showGraphDialog.java
@@ -0,0 +1,36 @@
+package MyJava;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class showGraphDialog extends JDialog {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public showGraphDialog()  {
+		Container container=getContentPane();
+		container.setLayout(new BorderLayout());
+		JLabel jl=new JLabel();
+		Icon icon=new ImageIcon("/Users/joker/Desktop/DotGraph.jpg");
+		jl.setIcon(icon);
+		JScrollPane sp = new JScrollPane();
+		sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		sp.setViewportView(jl);
+		container.add(sp);
+		//container.add(jl, BorderLayout.CENTER);
+		setVisible(true);
+		Toolkit kit=Toolkit.getDefaultToolkit();
+		Dimension screenSize=kit.getScreenSize();
+		setBounds(screenSize.width/2, screenSize.height/2,Math.min(icon.getIconWidth(),screenSize.width/2 ),screenSize.height/2);
+		setResizable(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+	public static void main(String[]args)  {
+
+	}
+}
